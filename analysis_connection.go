f5a11e9335f79b294dec6921f613ff336e4f651b
Halil Özaktaş <h.ozaktas@gmail.com>
2019-10-18 15:49:30 +0300

    clean

--- analysis_connection.go
+++ analysis_connection.go
@@ -2,99 +2,90 @@ package gorma
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"sort"
 	"strconv"
 	"strings"
+	"time"
 
+	"github.com/go-redis/redis"
 	"github.com/hto/redis-memory-analysis/storages"
 )
 
-type AnalysisConnection struct {
-	redis   *storages.RedisClient
-	Reports DBReports
-}
+var Reports = DBReports{}
+var redisClient *redis.Client
 
-func NewAnalysisConnection(host string, port uint16, password string) (*AnalysisConnection, error) {
-	redis, err := storages.NewRedisClient(host, port, password)
-	if err != nil {
-		return nil, err
+func Connection(host string, port string, password string) {
+	client := redis.NewClient(&redis.Options{
+		Addr:        host + ":" + port,
+		Password:    password, // no password set
+		DB:          0,        // use default DB
+		ReadTimeout: 1 * time.Minute,
+	})
+
+	pong, err := client.Ping().Result()
+
+	if err != nil || pong == "" {
+		log.Fatal("\n\nREDIS NOT CONNECT : ", err)
 	}
-	return &AnalysisConnection{redis, DBReports{}}, nil
+
+	redisClient = client
 }
 
-func (analysis *AnalysisConnection) Close() {
-	if analysis.redis != nil {
-		_ = analysis.redis.Close()
+func Close() {
+	if redisClient != nil {
+		redisClient.Close()
 	}
 }
 
-func (analysis AnalysisConnection) Start(delimiters []string) {
+func Start(delimiters []string) {
 	fmt.Println("Starting analysis")
 
-	databases, _ := analysis.redis.GetDatabases()
+	databases := GetDatabases()
 
 	var (
-		cursor uint64
-		r      Report
-		f      float64
-		ttl    int64
-		length uint64
-		sr     SortBySizeReports
-		mr     KeyReports
+		// r Report
+		// f      float64
+		// ttl    int64
+		sr SortBySizeReports
+		mr KeyReports
 	)
 
 	for db, keyCount := range databases {
+
 		fmt.Println("Analyzing db", db)
-		cursor = 0
 		mr = KeyReports{}
 
-		_ = analysis.redis.Select(db)
+		keys, _, err := redisClient.Scan(db, "*", keyCount).Result()
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Printf("Key Prefix : %s  ResultCount: %d\n", "*", len(keys))
 
-		for {
-			keys, _ := analysis.redis.Scan(&cursor, "*", keyCount)
-			groupKey := ""
-			for _, key := range keys {
-				for _, delimiter := range delimiters {
-					tmp := strings.Split(key, delimiter)
-					if len(tmp) > 1 {
-						groupKey = strings.Join(tmp[0:len(tmp)-1], delimiter) + delimiter + "*"
-						break
-					} else {
-						groupKey = key
-					}
-				}
-
-				if _, ok := mr[groupKey]; ok {
-					r = mr[groupKey]
+		groupKey := ""
+		for _, key := range keys {
+			for _, delimiter := range delimiters {
+				tmp := strings.Split(key, delimiter)
+				if len(tmp) > 1 {
+					groupKey = strings.Join(tmp[0:len(tmp)-1], delimiter) + delimiter + "*"
+					break
 				} else {
-					r = Report{groupKey, 0, 0, 0, 0}
+					groupKey = key
 				}
-
-				ttl, _ = analysis.redis.Ttl(key)
-
-				switch ttl {
-				case -2:
-					continue
-				case -1:
-					r.NeverExpire++
-					r.Count++
-				default:
-					f = float64(r.AvgTtl*(r.Count-r.NeverExpire)+uint64(ttl)) / float64(r.Count+1-r.NeverExpire)
-					ttl, _ := strconv.ParseUint(fmt.Sprintf("%0.0f", f), 10, 64)
-					r.AvgTtl = ttl
-					r.Count++
-				}
-
-				length, _ = analysis.redis.SerializedLength(key)
-				r.Size += length
-
-				mr[groupKey] = r
 			}
 
-			if cursor == 0 {
-				break
+			lenght, _ := redisClient.MemoryUsage(key).Result()
+
+			r := Report{}
+			if _, ok := mr[groupKey]; ok {
+				r = mr[groupKey]
+			} else {
+				r = Report{groupKey, 0, 0, 0, 0}
 			}
+			r.Size += uint64(lenght)
+			r.Count++
+			mr[groupKey] = r
 		}
 
 		//Sort by size
@@ -104,14 +95,12 @@ func (analysis AnalysisConnection) Start(delimiters []string) {
 		}
 		sort.Sort(sr)
 
-		analysis.Reports[db] = sr
-
-		// fmt.Println(analysis.Reports)
+		Reports[db] = sr
 	}
 }
 
-func (analysis AnalysisConnection) SaveReports(folder string) error {
-	// fmt.Println("Saving the results of the analysis into...", folder)
+func SaveReports(folder string) error {
+	fmt.Println("Saving the results of the analysis into...", folder)
 	if _, err := os.Stat(folder); os.IsNotExist(err) {
 		_ = os.MkdirAll(folder, os.ModePerm)
 	}
@@ -122,25 +111,55 @@ func (analysis AnalysisConnection) SaveReports(folder string) error {
 		size     float64
 		unit     string
 	)
-	template := fmt.Sprintf("%s%sredis-analysis-%s%s", folder, string(os.PathSeparator), strings.Replace(analysis.redis.Id, ":", "-", -1), "-%d.csv")
-	for db, reports := range analysis.Reports {
+	template := fmt.Sprintf("%s%sredis-analysis-%s%s", folder, string(os.PathSeparator), strings.Replace("analysis.redis.Id", ":", "-", -1), "-%d.csv")
+	for db, reports := range Reports {
 		filename = fmt.Sprintf(template, db)
 		fp, err := storages.NewFile(filename, os.O_CREATE|os.O_WRONLY, os.ModePerm)
 		if err != nil {
 			return err
 		}
-		_, _ = fp.Append([]byte("Key,Count,Size,NeverExpire,AvgTtl(excluded never expire)\n"))
+		_, _ = fp.Append([]byte("Key,Count,Size\n"))
 		for _, value := range reports {
 			size, unit = HumanSize(value.Size)
-			str = fmt.Sprintf("%s,%d,%s,%d,%d\n",
+			str = fmt.Sprintf("%s,%d,%s\n",
 				value.Key,
 				value.Count,
-				fmt.Sprintf("%0.3f %s", size, unit),
-				value.NeverExpire,
-				value.AvgTtl)
+				fmt.Sprintf("%0.3f %s", size, unit))
 			_, _ = fp.Append([]byte(str))
 		}
 		fp.Close()
 	}
 	return nil
+}
+
+func GetDatabases() map[uint64]int64 {
+
+	var databases = make(map[uint64]int64)
+	reply, _ := redisClient.Info("keyspace").Result()
+	keyspace := strings.Trim(reply[12:], "\n")
+	keyspaces := strings.Split(keyspace, "\r")
+
+	for _, db := range keyspaces {
+		dbKeysParse := strings.Split(db, ",")
+		if dbKeysParse[0] == "" {
+			continue
+		}
+
+		dbKeysParsed := strings.Split(dbKeysParse[0], ":")
+		dbNo, _ := strconv.ParseUint(dbKeysParsed[0][2:], 10, 64)
+		dbKeySize, _ := strconv.ParseInt(dbKeysParsed[1][5:], 10, 64)
+		databases[dbNo] = dbKeySize
+	}
+	return databases
+}
+
+func HumanSize(byte uint64) (float64, string) {
+	units := []string{"Bytes", "KB", "MB", "GB", "TB", "PB", "EB"}
+	fb := float64(byte)
+	i := 0
+	for ; fb >= 1024; i++ {
+		fb /= 1024
+	}
+	size, _ := strconv.ParseFloat(fmt.Sprintf("%0.3f", fb), 64)
+	return size, units[i]
 }
--- go.mod
+++
@@ -1,6 +0,0 @@
-module github.com/hto/redis-memory-analysis
-
-require (
-	github.com/garyburd/redigo v1.6.0
-	github.com/vrischmann/rdbtools v0.0.0-20141203205512-cd9eb17adda8
-)
